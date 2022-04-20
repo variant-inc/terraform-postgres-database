a4a2173ffca6b3b6a7678269adf6d0f9af69de8b
chriskvariant <chrisk@drivevariant.com>
2022-04-20 12:36:42 -0400

    Added resources for SecretManager secrets.

--- main.tf
+++ main.tf
@@ -46,3 +46,45 @@ resource "postgresql_extension" "my_extension" {
   name     = each.key
   database = var.database_name
 }
+
+# Rotating secret START #
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+resource "aws_secretsmanager_secret" "database_credentials" {
+  #Will Default:
+  #kms_key_id  = var.kms_key_id
+
+  description = "${var.database_name} database secret key."
+  name = "${var.database_name}-postgres-secret"
+}
+
+resource "aws_secretsmanager_secret_rotation" "database_credentials" {
+  rotation_lambda_arn = "arn:aws:lambda:${data.aws-region.current}:${data.aws_caller_identity.current}:function:rds_key_rotation"
+  secret_id           = aws_secretsmanager_secret.database_credentials.id
+
+  rotation_rules {
+    automatically_after_days = 30 #var.rotation_interval
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "database_credentials" {
+  secret_id     = aws_secretsmanager_secret.database_credentials.id
+  secret_string = jsonencode({
+    "password" = random_password.role_password.result # Password will be rotated immediately upon creation
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to the credential info. After creation, any changes to
+      # non-password fields should be made through the console to avoid issues
+      # with Terraform resetting the password on the secret but not updating the
+      # password on RDS.
+      secret_string,
+    ]
+  }
+}
+
+# Rotating Secret END #
